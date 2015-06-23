b8b79c0ea7552e975ee18aa24ae9404ce5c5fed1
bmanley <bmanley@commercehub.com>
2015-06-23 15:04:13 -0400

    Updated .bat files because Windows HAS to be different.

--- CalculatorTests/v1/checkout/cucumber.bat
+++ CalculatorTests/v1/checkout/cucumber.bat
@@ -1,2 +1,2 @@
 javac -cp "jars/*" step_definitions/CheckoutSteps.java
-java -cp "jars/*:." cucumber.api.cli.Main -p pretty --snippets camelcase  -g step_definitions features
+java -cp "jars/*" cucumber.api.cli.Main -p pretty --snippets camelcase  -g step_definitions features
